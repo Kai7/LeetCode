c544c6b6138f14ce996dd68eca8a5b6e8dd2a849
Kai7 <kaiseven.yang@gmail.com>
2021-03-22 22:07:32 +0800

    add -fsanitize=undefined in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-CXX_FLAG="-std=c++17 -Wall -Wextra -Werror -fsanitize=address"
+CXX_FLAG="-std=c++17 -Wall -Wextra -Werror -fsanitize=address -fsanitize=undefined"
 g++ ${CXX_FLAG} main.cpp -o main -I ..; ./main; rm main
 
-g++ -std=c++17 -Wall -Wextra -fsanitize=address main.cpp -o main -I ..; ./main; rm main
+g++ -std=c++17 -Wall -Wextra -fsanitize=address -fsanitize=undefined main.cpp -o main -I ..; ./main; rm main
