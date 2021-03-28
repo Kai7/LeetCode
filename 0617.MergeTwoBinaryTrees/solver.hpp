2bb51cadf9b1d6fb94cc7ce2566d9ad603451ad6
Kai7 <kaiseven.yang@gmail.com>
2021-03-28 21:43:05 +0800

    speed up 0617-(A,83,96)

--- 0617.MergeTwoBinaryTrees/solver.hpp
+++ 0617.MergeTwoBinaryTrees/solver.hpp
@@ -3,6 +3,18 @@
 
 class Solution {
 public:
+  /* Accepted: 83.32, 96.70 */
+  TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+    if (!root1) return root2;
+    if (!root2) return root1;
+    root1->left = mergeTrees(root1->left, root2->left);
+    root1->right = mergeTrees(root1->right, root2->right);
+    root1->val += root2->val;
+    delete root2;
+    return root1;
+  }
+
+#if 0
   /* Accepted: 64.56, 60.37 */
   TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
     if (root1 == nullptr) return root2;
@@ -13,4 +25,5 @@ public:
     delete root2;
     return root1;
   }
+#endif 
 };
